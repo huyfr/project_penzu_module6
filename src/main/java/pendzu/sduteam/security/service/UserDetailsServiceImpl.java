c6d8fc9098639d118c77be8f7b86278be67da43b
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-07-21 17:33:51 +0700

    Add Service Security

---
+++ src/main/java/pendzu/sduteam/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package pendzu.sduteam.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pendzu.sduteam.models.User;
+import pendzu.sduteam.repositories.IUserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("User Not Found with -> username or email : " + username));
+
+        return UserPrinciple.build(user);
+    }
+}
