5d8462b3461b355570662df333a6023f5103877a
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-07-21 19:19:17 +0700

    Add Service IRole + IUser

---
+++ src/main/java/pendzu/sduteam/services/IUserService.java
@@ -0,0 +1,14 @@
+package pendzu.sduteam.services;
+
+import pendzu.sduteam.models.User;
+
+import java.util.Optional;
+
+public interface IUserService extends GenericService<User> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByEmail(String email);
+    Boolean existsByUsername(String username);
+
+    Iterable<User> findUsersByNameContaining(String user_name);
+}
