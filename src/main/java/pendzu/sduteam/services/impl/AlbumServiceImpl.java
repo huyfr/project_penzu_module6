2d2ff45a2e17bdf169008f4e901e2aeea507b6ba
Bui Xuan huynh <huynhxuanbui@gmail.com>
2020-07-29 23:47:52 +0700

    create repository and service to get all album by userId and status

--- src/main/java/pendzu/sduteam/services/impl/AlbumServiceImpl.java
+++ src/main/java/pendzu/sduteam/services/impl/AlbumServiceImpl.java
@@ -1,17 +1,24 @@
 package pendzu.sduteam.services.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pendzu.sduteam.models.Album;
+import pendzu.sduteam.models.Image;
 import pendzu.sduteam.repositories.IAlbumRepository;
 import pendzu.sduteam.services.IAlbumService;
 
 import java.util.Optional;
 
 @Service
+@PropertySource({"classpath:status.properties"})
 public class AlbumServiceImpl implements IAlbumService {
+
+  @Value("${entity.deleted}")
+  private int deleteStatus;
 
   @Autowired
   private IAlbumRepository albumRepository;
@@ -28,21 +35,26 @@ public class AlbumServiceImpl implements IAlbumService {
 
   @Override
   public Optional<Album> findById(Long id) {
-    return Optional.empty();
+    return this.albumRepository.findById(id);
   }
 
   @Override
   public Iterable<Album> findAll() {
-    return null;
+    return this.albumRepository.findAll();
   }
 
   @Override
   public Album save(Album model) {
-    return null;
+    return this.albumRepository.save(model);
   }
 
   @Override
   public void delete(Long id) {
-
+    Optional<Album> albumOptional = this.albumRepository.findById(id);
+    Album album = albumOptional.get();
+    if (album != null) {
+      album.setStatus(deleteStatus);
+      this.albumRepository.save(album);
+    }
   }
 }
