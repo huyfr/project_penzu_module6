e9808377ec7f79032485f31be44a017e7ae48e88
Bui Xuan huynh <huynhxuanbui@gmail.com>
2020-07-29 23:49:53 +0700

    create method get all album by userId and status in AlbumRestAPIs, test Api ok

--- src/main/java/pendzu/sduteam/controllers/AlbumRestAPIs.java
+++ src/main/java/pendzu/sduteam/controllers/AlbumRestAPIs.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pendzu.sduteam.models.Album;
+import pendzu.sduteam.services.IAlbumService;
 import pendzu.sduteam.services.impl.AlbumServiceImpl;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/sdu")
@@ -30,6 +32,15 @@ public class AlbumRestAPIs {
   @Autowired
   private AlbumServiceImpl albumService;
 
+  @GetMapping("/album")
+  public ResponseEntity<List<Album>>getAllAlbum(){
+    List<Album> albums = (List<Album>) this.albumService.findAll();
+    if (albums.isEmpty()){
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    return new ResponseEntity<>(albums, HttpStatus.OK);
+  }
+
   @GetMapping("/album/userId/{id}")
   public ResponseEntity<?> getAlbumsByUserIdAndStatus(@PathVariable Long id) {
     List<Album> albums = (List<Album>) albumService.findAllByUserIdAndStatusAfter(id,allPostAvailable);
@@ -38,5 +49,15 @@ public class AlbumRestAPIs {
     }
 
     return new ResponseEntity<>(albums, HttpStatus.OK);
+  }
+
+  @GetMapping("/album/{id}")
+  public ResponseEntity<?>getAlbumById(@PathVariable("id")Long id) {
+    Optional<Album> album = this.albumService.findById(id);
+
+    if (!album.isPresent()){
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<>(album,HttpStatus.OK);
   }
 }
