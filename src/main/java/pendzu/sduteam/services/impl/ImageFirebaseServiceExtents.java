26c2f0a0f7be0f6bb07ead3c7cedeae7cf0affc8
Bui Xuan huynh <huynhxuanbui@gmail.com>
2020-07-30 00:04:23 +0700

    connect ImageFirebaseServiceExtents and AlbumFirebaseService to FireBase Storage, get detail allbum api ok

---
+++ src/main/java/pendzu/sduteam/services/impl/ImageFirebaseServiceExtents.java
@@ -0,0 +1,9 @@
+package pendzu.sduteam.services.impl;
+
+import org.springframework.stereotype.Service;
+import pendzu.sduteam.models.Image;
+import pendzu.sduteam.services.FirebaseStorageService;
+
+@Service
+public class ImageFirebaseServiceExtents extends FirebaseStorageService<Image> {
+}
